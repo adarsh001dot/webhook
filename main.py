b2775e58deec27d8d25bafb93e578dd36fc875cb
adarsh001dot <harshitsaxena5918@gmail.com>
2026-03-15 19:06:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ import hashlib
 
 # ==================== CONFIGURATION ====================
 MONGODB_URI = "mongodb+srv://nikilsaxena843_db_user:3gF2wyT4IjsFt0cY@vipbot.puv6gfk.mongodb.net/?appName=vipbot"
-BOT_TOKEN = "8612834168:AAFT1VX35aEpyEOMoszHf2ymrr2R4iP3gvQ"
+BOT_TOKEN = "8294367270:AAFbCzMXn3vTAcYxCNeOMPOdwtcSN8GpQnE"
 OWNER_ID = 7459756974
 OWNER_USERNAME = "@VIP_X_OFFICIAL"
 
